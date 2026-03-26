8c8617533934dd4cba21f12a254abd4ce9008ece
benk2001 <Benkapepula@gmail.com>
2026-03-26 16:13:42 +0200

    Update tableaux.php

--- tableaux.php
+++ tableaux.php
@@ -11,7 +11,7 @@
             font-family: 'Inter', sans-serif;
             /* On reprend le même fond que l'accueil */
             background: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), 
-                        url('images/frontimage.jpg');
+                        url('frontimage.jpg');
             background-size: cover;
             background-position: center;
             background-attachment: fixed;
